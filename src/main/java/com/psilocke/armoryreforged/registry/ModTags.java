f8da673f6dc6441720d6d429444b5391bef5bc0b
Psilocke <rhysculley10@gmail.com>
2022-05-10 00:07:17 -0500

    added mod tags

--- src/main/java/com/psilocke/armoryreforged/registry/ModTags.java
+++ src/main/java/com/psilocke/armoryreforged/registry/ModTags.java
@@ -5,7 +5,7 @@ import com.psilocke.armoryreforged.ArmoryReforged;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
-import net.minecraft.tags.Tag.Named;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
@@ -13,24 +13,24 @@ public class ModTags {
 	
 	public static final class Blocks {
 
-		private static Named<Block> forge(String path) {
-			return BlockTags.bind(new ResourceLocation("forge", path).toString());
+		private static TagKey<Block> forge(String path) {
+			return BlockTags.create(new ResourceLocation("forge", path));
 		}
 		
-		private static Named<Block> mod(String path) {
-			return BlockTags.bind(new ResourceLocation(ArmoryReforged.MOD_ID, path).toString());
+		private static TagKey<Block> mod(String path) {
+			return BlockTags.create(new ResourceLocation(ArmoryReforged.MOD_ID, path));
 		}
 	}
     
 	public static final class Items {
-		public static final Named<Item> TOOL_HAMMER = forge("tools/hammer");
+		public static final TagKey<Item> TOOL_HAMMER = forge("tools/hammer");
 
-		private static Named<Item> forge(String path) {
-			return ItemTags.bind(new ResourceLocation("forge", path).toString());
+		private static TagKey<Item> forge(String path) {
+			return ItemTags.create(new ResourceLocation("forge", path));
 		}
 		
-		private static Named<Item> mod(String path) {
-			return ItemTags.bind(new ResourceLocation(ArmoryReforged.MOD_ID, path).toString());
+		private static TagKey<Item> mod(String path) {
+			return ItemTags.create(new ResourceLocation(ArmoryReforged.MOD_ID, path));
 		}
 	}
 }
